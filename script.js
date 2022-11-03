c71d4672da6cc69cf8ad4dd3e8417d0af90d057e
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-11-03 11:10:22 -0300

    commit-bug

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 //DOM, JAVASCRIPT... (Modelo de Documento por Objetos).
 
+const toDoField = document. querySelector('.to-do-field')
+const toDoSend = document. querySelector ('.to-do-send')
+const toDoList = document. querySelector ('.to-do-list')
 
+toDoSend.addEventListener('click', function(e){ 
+    const li = document.createElement('li');
+    const buttonClear = document.createElement('button');
+    buttonClear.setAttribute('class', 'buttonclear');
+    buttonClear.innerText = 'deletar';
+    li.innerText = toDoField.value;
+    li.appendChild(buttonClear);
+    toDoList.appendChild(li);
+})
+
+addEventListener('click',function(e){
+    const el = e.target;
+
+    if(el.classList.contains('button-clear')){
+        el.parentElement.remove()
+    }
+}
