0d926954cdf18a9ef8584c0c6a02395c9410eed7
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-11-10 10:00:24 -0300

    10am10112022

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-//DOM, JAVASCRIPT... (Modelo de Documento por Objetos).
+const toDoField = document.querySelector('.to-do-field')
+const toDoSend = document.querySelector('.to-do-send')
+const toDoList = document.querySelector('.to-do-list')
 
-const toDoField = document. querySelector('.to-do-field')
-const toDoSend = document. querySelector ('.to-do-send')
-const toDoList = document. querySelector ('.to-do-list')
-
-toDoSend.addEventListener('click', function(e){ 
+toDoSend.addEventListener('click',function(e){
     const li = document.createElement('li');
     const buttonClear = document.createElement('button');
-    buttonClear.setAttribute('class', 'buttonclear');
-    buttonClear.innerText = 'deletar';
+    buttonClear.setAttribute ('class', 'button-clear');
+    buttonClear.innerText = 'Deletar'
     li.innerText = toDoField.value;
     li.appendChild(buttonClear);
     toDoList.appendChild(li);
@@ -17,7 +15,7 @@ toDoSend.addEventListener('click', function(e){
 addEventListener('click',function(e){
     const el = e.target;
 
-    if(el.classList.contains('button-clear')){
+    if(el.classList.contains ('button-clear')){
         el.parentElement.remove()
     }
-}
+})
