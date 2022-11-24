838b431cd12c6eb002e38bfc64df4cce0a47ca5e
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-11-24 08:20:35 -0300

    commit 24112022-0820am

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ const toDoField = document.querySelector('.to-do-field')
 const toDoSend = document.querySelector('.to-do-send')
 const toDoList = document.querySelector('.to-do-list')
 
+
 function criaLi(){
     const li = document.createElement('li');
     return li;
-}
+};
 
 function criaButtonClear(){
     const buttonClear = document.createElement('button');
     buttonClear.setAttribute ('class', 'button-clear fa fa-trash');
     return buttonClear;
-}
+};
 
 function criaTarefa(li, buttonClear){
     li.innerText = toDoField.value;
     li.appendChild(buttonClear);
     toDoList.appendChild(li);
-}
+};
 
 toDoSend.addEventListener('click',function(e){
     const li = criaLi();
@@ -29,10 +30,10 @@ toDoField.addEventListener('keyup', function(e){
     if(e.keyCode === 13){
         const li = criaLi();
         const buttonClear = criaButtonClear();
-        const tarefa = criaTarefa(li, buttonClear);    
+        const tarefa = criaTarefa(li, buttonClear);  0
     }
 
-})
+});
 
 addEventListener('click',function(e){
     const el = e.target;
